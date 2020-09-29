82dd9c2273ba9dca7e210f5fa8625654657afb07
= <=>
2020-09-30 02:29:19 +0600

    Basic setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YTube</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/all.css">
+    
+    <script src="assets/bootstrap/jquery.js"></script>
+    <script src='assets/bootstrap/popper.js'></script>
+    <script src="assets/bootstrap/bootstrap.js"></script>
+    <!-- Jquery -->
+    <script src="assets/js/jQuery.js"></script>
+    <!-- Js scripts -->
+    <script src="assets/js/script.js"></script>
+    
+    <!-- css links -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
+<body>
+    <h1 class="text-danger">Youtube Clone PHP</h1>
+</body>
+</html>
