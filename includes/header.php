e8e3e94dfa1690083b55117047ef6e07adff8108
= <=>
2020-09-30 19:40:29 +0600

    Top bar

---
+++ includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YTube</title>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/all.css">
+    
+    <script src="assets/bootstrap/jquery.js"></script>
+    <script src='assets/bootstrap/popper.js'></script>
+    <script src="assets/bootstrap/bootstrap.js"></script>
+    <!-- Jquery -->
+    <script src="assets/js/jQuery.js"></script>
+    <!-- Js scripts -->
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/commonActions.js"></script>
+
+    <!-- css links -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
+<body>
+<div id="pageContainer">
+        <div id="headContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png" alt="brand"/>
+            </button>
+            <a href="index.php" class="logo">
+                <img src="assets/images/icons/VideoTubeLogo.png" alt="sitelogo">
+            </a>
+            <div class="searchBar">
+                <form action="" method="get">
+                    <input type="text" placeholder="search..." name="term" class="search" />
+                    <button class="searchBtn">
+                        <img src="assets/images/icons/search.png" />
+                    </button>
+                </form>
+            </div>
+            <div class="rightIcons">
+                <a href="">
+                <img class="upload" src="assets/images/icons/upload.png">
+                </a>
+                <a href="">
+                <img class="upload" src="assets/images/profilePictures/default.png">
+                </a>
+            </div>
+        </div>
+        <div id="sidenavContainer" style="display: none;">
+        hello 2
+        </div>
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
--- index.php
+++ index.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YTube</title>
 
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
-    <link rel="stylesheet" href="assets/bootstrap/all.css">
-    
-    <script src="assets/bootstrap/jquery.js"></script>
-    <script src='assets/bootstrap/popper.js'></script>
-    <script src="assets/bootstrap/bootstrap.js"></script>
-    <!-- Jquery -->
-    <script src="assets/js/jQuery.js"></script>
-    <!-- Js scripts -->
-    <script src="assets/js/script.js"></script>
-    
-    <!-- css links -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-</head>
-<body>
-    <h1 class="text-danger">Youtube Clone PHP</h1>
-</body>
-</html>
+<?php include("includes/header.php") ?>  
+        
+    hello 3
+
+<?php require_once("includes/footer.php") ?>
+           
